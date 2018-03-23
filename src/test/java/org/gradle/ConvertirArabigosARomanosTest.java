f9838da5ccc925ae3df7dc4bba04d85f0bb898d5
Jhon Perez <jrperez175@gmail.com>
2018-03-23 14:32:15 -0500

    Commit 23marzo2018

--- src/test/java/org/gradle/ConvertirArabigosARomanosTest.java
+++ src/test/java/org/gradle/ConvertirArabigosARomanosTest.java
@@ -1,8 +1,8 @@
 package org.gradle;
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert;
-import static org.junit.Assert.*;
 
 public class ConvertirArabigosARomanosTest {
 	
